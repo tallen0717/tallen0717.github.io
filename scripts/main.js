c5e76e6a72eb1c0cdb77a3960353c756ff37355f
tallen0717 <90996500+tallen0717@users.noreply.github.com>
2021-09-19 17:44:14 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,59 @@
+//let myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello world!';
+
+/*let myVariable;
+myVariable='李雷';*/
+//let myVariable='李雷';
+//let iceCream = 'chocolate';
+
+/*if (iceCream === 'chocolate') {
+  alert('我最喜欢巧克力冰激淋了。');
+} else {
+  alert('但是巧克力才是我的最爱呀……');
+}*/
+
+//let myVariable = document.querySelector('h1');
+//alert('hello!');
+
+/*function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+  } 
+  
+multiply(4, 7);*/
+
+/*document.querySelector('html').onclick = function() {
+    alert('别戳我，我怕疼。');
+}*/
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/JBB.jpg') {
+      myImage.setAttribute('src', 'images/tz.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/JBB.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName || myName === 'null') {
+    setUserName();
+  } else {
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();  
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
